8b5cae629a67b202b9e0f0087d96cb89216a3d6e
Md.Nafiz Ahammed <63756922+nafizahammed@users.noreply.github.com>
2021-09-25 20:19:49 -0700

    Add files via upload

---
+++ Task1/LabTask1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+
+	<h1>Registration form </h1>
+    First Name:<input type="text" name="fname"> 
+    <br>
+    <br>
+    Last Name:<input type="text" name="lname"> 
+    <br>
+    <br>
+    Age:<input type="text" name="age">
+    <br>
+    <br>
+
+     <p>Designation:</p>
+     <input type="radio" id="junion programmer" name="designation" value="junion programmer">
+     <label for="junion programmer">Junion Programmer</label>
+
+     <input type="radio" id="senior programmer" name="designation" value="senior programmer">
+     <label for="senior programmer">Senior Programmer</label>
+     
+     <input type="radio" id="project lead" name="designation" value="project lead">
+     <label for="project lead">Project Lead</label>
+    
