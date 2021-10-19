b4352afb6bc3ad1413eb048e79c9bb88d04948ba
Md.Nafiz Ahammed <63756922+nafizahammed@users.noreply.github.com>
2021-10-19 09:50:37 +0600

    Add files via upload

---
+++ Lab_Exam/new_students.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2021 at 05:39 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aiub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `new_students`
+--
+
+CREATE TABLE `new_students` (
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` text NOT NULL,
+  `mnum` int(11) NOT NULL,
+  `dob` date NOT NULL,
+  `hr` float NOT NULL,
+  `sr` float NOT NULL,
+  `course` text NOT NULL,
+  `year` int(10) NOT NULL,
+  `semester` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
